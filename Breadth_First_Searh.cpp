9346d41ce794a1003ab52eb0870fcab61b25f5b9
Xin (Chris) Zou <x9zou@uwaterloo.ca>
2015-02-19 21:28:35 -0500

    Update Breadth_First_Searh.cpp

--- Breadth_First_Searh.cpp
+++ Breadth_First_Searh.cpp
@@ -6,3 +6,13 @@ queue< Binary_tree_node<Object> * > que;
 
 // push the root onth the queue 
 que.enqueue( root );
+
+// Push all of its children of the front node onto the queue
+while ( !que.empty() ) {
+Binary_tree_node<Object> *ptr = que.dequeue(); if ( left() != 0 ) {
+que.enqueue( left() ); }
+if ( right() != 0 ) {
+que.enqueue( right() ); }
+
+
+}
