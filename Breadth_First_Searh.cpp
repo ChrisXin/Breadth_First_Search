5a6d9c5e9e1b967cd09e3b31541608cca08062a1
Xin (Chris) Zou <x9zou@uwaterloo.ca>
2015-02-19 21:29:50 -0500

    Update Breadth_First_Searh.cpp

--- Breadth_First_Searh.cpp
+++ Breadth_First_Searh.cpp
@@ -9,10 +9,13 @@ que.enqueue( root );
 
 // Push all of its children of the front node onto the queue
 while ( !que.empty() ) {
-Binary_tree_node<Object> *ptr = que.dequeue(); if ( left() != 0 ) {
-que.enqueue( left() ); }
-if ( right() != 0 ) {
-que.enqueue( right() ); }
+     Binary_tree_node<Object> *ptr = que.dequeue(); if ( left() != 0 ) {
+     que.enqueue( left() ); 
+     }
+     if ( right() != 0 ) {
+     que.enqueue( right() );
+     }
 
-
+// print it out
+  std::cout << ptr->retrieve() << std::endl; }
 }
